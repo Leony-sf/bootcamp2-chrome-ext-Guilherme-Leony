8d52d6de2a0bd16b0b75b5a6e182d6df74dbc9d4
Leony-sf <guilherme.leony@sempreceub.com>
2025-10-19 13:49:08 -0300

    Update playwright.config.ts

--- tests/playwright.config.ts
+++ tests/playwright.config.ts
@@ -1,25 +1,24 @@
-// tests/playwright.config.ts
+
 import { defineConfig, devices } from '@playwright/test';
 import path from 'node:path';
-import { fileURLToPath } from 'node:url'; // <-- CORREÇÃO (ADICIONADO)
+import { fileURLToPath } from 'node:url'; 
 
-// --- Correção para __dirname em ESM ---
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-// --- Fim da Correção ---
 
-// Define o caminho para a pasta 'dist' que criamos no Passo 1
-const distPath = path.join(__dirname, '..', 'dist'); // <-- Esta linha agora funciona
 
+
+const distPath = path.join(__dirname, '..', 'dist'); 
 export default defineConfig({
-  // O diretório onde seus arquivos de teste estão
+
   testDir: __dirname,
   
-  // Onde salvar os relatórios
+
   reporter: [['list'], ['html', { outputFolder: 'playwright-report' }]],
   
   use: {
-    // Rodar os testes sem abrir a janela do navegador (mais rápido para CI)
+   
     headless: true,
   },
   
@@ -29,7 +28,7 @@ export default defineConfig({
       use: {
         ...devices['Desktop Chrome'],
         launchOptions: {
-          // Argumentos essenciais para carregar a extensão
+      
           args: [
             `--disable-extensions-except=${distPath}`,
             `--load-extension=${distPath}`
